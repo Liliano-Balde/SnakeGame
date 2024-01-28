a51f7e984d4c3f9c626316e1af4435446f37caf4
Liliano-Balde <liliano.balde@hotmail.com>
2024-01-28 20:46:14 +0000

    initial commmit

---
+++ src/GameFrame.java
@@ -0,0 +1,10 @@
+package snakepackage;
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+	GameFrame() {
+
+	}
+
+}
---
+++ src/GamePanel.java
@@ -0,0 +1,20 @@
+package snakepackage;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+//implemented action listener which is used to handel events, in this case clicks
+public class GamePanel extends JPanel implements ActionListener {
+
+	GamePanel() {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
