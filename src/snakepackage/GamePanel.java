5d88f5030aeb7aa0890ae2aa9ac46c862e54cfd9
Liliano-Balde <liliano.balde@hotmail.com>
2024-01-29 01:19:06 +0000

    Created GamePannel Class
    Implemented game panel class with initial structure.
    extended JPanel and implemented ActionListener interface.
    added constants, arrays, initial state variables and methods for
    drawing, moving, checking collisions and handling key events.

---
+++ src/snakepackage/GamePanel.java
@@ -0,0 +1,128 @@
+package snakepackage;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+import java.util.Timer;
+
+import javax.swing.JPanel;
+
+//implemented action listener which is used to handel events, in this case clicks
+public class GamePanel extends JPanel implements ActionListener {
+
+	// Constants for screen dimensions, unit size, game units, delay
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	static final int UNIT_SIZE = 20;
+	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+	static final int DELAY = 75;
+
+	// Arrays that will store coordinates of snake body parts (keep track of where
+	// each part of the snake on the screen)
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+
+	// Initial state variables (starting settings of the game i.e size of snake, if
+	// the game is running etc)
+	int bodyParts = 2;
+	int applesEaten;
+	int appleX;
+	int appleY;
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
+
+	GamePanel() {
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+
+	}
+
+	// Method to initialize the game state
+	public void startGame() {
+		newApple();
+		running = true;
+		timer = new Timer(DELAY, this);
+		timer.start();
+	}
+
+//	this overide will customize drawing on the panel (how things are drawn on the panel)
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
+
+//	Method to draw the game elements grid and apple (basically ahndles the visuals like the grid and the apple)
+	public void draw(Graphics g) {
+		for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+			g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+		}
+		g.setColor(Color.red);
+		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+	}
+
+	// Method that generates a random apple
+	public void newApple() {
+		appleX = random.nextInt(SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE;
+		appleY = random.nextInt(SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+	}
+
+	// Method to move the snake around
+	public void move() {
+		for (int i = bodyParts; i > 0; i--) {
+			x[i] = x[i - 1];
+			y[i] = y[i - 1];
+		}
+		switch (direction) {
+		case 'U':
+			y[0] = y[0] - UNIT_SIZE;
+			break;
+		case 'D':
+			y[0] = y[0] + UNIT_SIZE;
+			break;
+		case 'L':
+			x[0] = x[0] - UNIT_SIZE;
+			break;
+		case 'R':
+			x[0] = x[0] + UNIT_SIZE;
+			break;
+
+		}
+
+	}
+
+	public void checkApple() {
+	}
+
+	public void checkCollisions() {
+	}
+
+	public void gameOver(Graphics g) {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public class MyKeyAdapter extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+
+		}
+	}
+
+}
